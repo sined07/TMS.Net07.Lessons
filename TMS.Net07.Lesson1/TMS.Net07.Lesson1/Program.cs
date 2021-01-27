6e2a96bdcd42091804a81d3d2a772a62cf756588
sergey <bystrov92@gmail.com>
2021-01-27 19:19:46 +0300

    add: lesson1

---
+++ TMS.Net07.Lesson1/TMS.Net07.Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.Net07.Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
